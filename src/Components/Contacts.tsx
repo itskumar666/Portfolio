38c5086cb2a136c7925f7dac55ecb4780debbbbb
itskumar666 <ashutosh666be@Gmail.com>
2024-06-13 00:52:09 +0530

    added contacts

--- src/Components/Contacts.tsx
+++ src/Components/Contacts.tsx
@@ -1,5 +1,57 @@
+import x from '../assets/x.png'
+import twitters from '../assets/twitters.png'
+import twitterm from '../assets/twitterm.png'
+import linkdins from '../assets/linkdins.png'
+import linkdinm from '../assets/linkdinm.png'
+import gits from '../assets/gits.png'
+import gitm from '../assets/gitm.png'
+import igs from '../assets/igs.png'
+import phone from "../assets/phone.png"
+import gmail from "../assets/gmail.png"
+import { useState } from 'react'
+
 export const Contacts=()=>{
+    const [gt,setGt]=useState(false)
+    const [pt,setPt]=useState(false)
+
+    const gmailtoggle=()=>{
+           setGt(!gt)
+    }
+    const pht=()=>{
+        setPt(!pt)
+    }
+
     return (
-        <div>Contacts</div>
+        <div>
+            <div className='grid grid-rows-3 md:grid-cols-3 md: justify-items-center md:gap-14 grid-cols-2 gap-y-10 mt-16'>
+                <div className='flex flex-row '>
+                  {!gt && <img src={gmail} onClick={gmailtoggle} alt="gmail" />}
+                  {gt && <p onClick={gmailtoggle} className='text-center justify-self-auto'>ashutosh666be@gmail.com</p> }
+
+                </div>
+                <div >
+                  {!pt && <img src={phone} onClick={pht} alt="number" />}
+                  {pt && <p onClick={pht} className='text-center'>+91-9621802295</p> }
+
+                </div>
+                 <div>
+                   
+                  <a href="https://github.com/itskumar666"> <img src={gits} alt="" />  Github</a>
+                </div> <div>
+                  <a href="https://x.com/itskumar666"> <img src={twitters} alt="" />Twitter</a>
+                </div> <div>
+                
+                  <a href="https://www.linkedin.com/in/ashutosh-kumar-677a64237/">   <img src={linkdins} alt="" /> Linkdin</a>
+                </div>
+                <div>
+                  <img src={igs} alt="" />
+                  <a href="">Instagram</a>
+                </div>
+                
+                
+
+            </div>
+            <div></div>
+        </div>
     )
 }
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,6 +2,15 @@ import { useState} from 'react';
 // import logo from '../assets/logo.png'; 
 import menu from '../assets/menu.png'; 
 import logo2 from '../assets/logo2.png'
+import x from '../assets/x.png'
+import twitters from '../assets/twitters.png'
+import twitterm from '../assets/twitterm.png'
+import linkdins from '../assets/linkdins.png'
+import linkdinm from '../assets/linkdinm.png'
+import gits from '../assets/gits.png'
+import gitm from '../assets/gitm.png'
+
+
 import { useNavigate } from 'react-router-dom';
 
 export const Header = () => {
@@ -41,35 +50,35 @@ const nav=()=>{
         <div className="flex justify-between text-white bg-black py-2">
             
             <div className='w-14 items-center ml-10 rounded-full md:ml-28'>
-                <img src={logo2} className=" hover:brightness-50 brightness-75 "onClick={nav} alt="" />
+                <img src={logo2} className=" hover:brightness-150 brightness-100 "onClick={nav} alt="" />
             </div>
           
             <div className='hidden md:flex justify-between   items-center text-xl mr-5 lg:mr-28 gap-14'>
                 
-            <h2 className="hover:text-green-100" onClick={handleportfolio}>Portfolio</h2>
-            <h2 className="hover:text-green-100" onClick={handleskills}>Skills</h2>
-                <h2 className="hover:text-green-100" onClick={handleBlogsClick}>Blogs</h2>
-                <h2 className="hover:text-green-100" onClick={handleContactsClick}>Contact</h2>
-                <a href="https://github.com/itskumar666" className="hover:text-green-100">GitHub</a>
-                <a href="https://www.linkedin.com/in/ashutosh-kumar-677a64237/" className="hover:text-green-100">LinkedIn</a>
-                <a href="https://x.com/itskumar666" className="hover:text-green-100">Twitter</a>
+            <h2 className="hover:hover:text-gray-700" onClick={handleportfolio}>Portfolio</h2>
+            <h2 className="hover:hover:text-gray-700" onClick={handleskills}>Skills</h2>
+                <h2 className="hover:text-gray-700" onClick={handleBlogsClick}>Blogs</h2>
+                <h2 className="hover:text-gray-700" onClick={handleContactsClick}>Contact</h2>
+                <a href="https://github.com/itskumar666" ><img className="hover:brightness-50" src={gits} alt="Github" /></a>
+                <a href="https://www.linkedin.com/in/ashutosh-kumar-677a64237/" ><img className="hover:brightness-50 " src={linkdins} alt="" /></a>
+                <a href="https://x.com/itskumar666" ><img className="hover:brightness-50" src={twitters} alt="" /></a>
             </div>
             <div className='md:hidden mr-2'>
               
               {!toggle &&
-               <img onClick={togglehandler} className=" rounded-full bg-stone-300" src={menu} alt="" />}
+               <img onClick={togglehandler} className=" " src={menu} alt="" />}
                {toggle && 
                <div className='grid grid-rows-12 h-screen text-white mr-10 '>
-                <button  onClick={togglehandler} className=''>X</button>
+                <button  onClick={togglehandler} className=''><img className='' src={x} alt="" /></button>
                 <div className=''></div>
                 
                 <h2 className="hover:text-black" onClick={handleportfolio}>Portfolio</h2>
                 <h2 className="hover:text-black" onClick={handleskills}>Skills</h2>
                 <h2 className="hover:text-black" onClick={handleBlogsClick}>Blogs</h2>
                 <h2 className="hover:text-black" onClick={handleContactsClick}>Contact</h2>
-                <a className="hover:text-white" href="https://github.com/itskumar666" >GitHub</a>
-                <a href="https://www.linkedin.com/in/ashutosh-kumar-677a64237/" className="hover:text-white">LinkedIn</a>
-                <a href="https://x.com/itskumar666">Twitter</a>
+                <a href="https://github.com/itskumar666" ><img className="hover:brightness-50" src={gits} alt="Github" /></a>
+                <a href="https://www.linkedin.com/in/ashutosh-kumar-677a64237/" ><img className="hover:brightness-150 " src={linkdins} alt="" /></a>
+                <a href="https://x.com/itskumar666" ><img className="hover:brightness-200" src={twitters} alt="" /></a>
 
                </div>
                }
