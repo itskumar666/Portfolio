9277fd9e9f90c10b81dcfa4dbec4d02cb7d3873d
itskumar666 <ashutosh666be@Gmail.com>
2024-06-12 07:56:00 +0530

    css complete

--- src/Components/Experienced.tsx
+++ src/Components/Experienced.tsx
@@ -11,19 +11,20 @@ export const ExperiencePage = () => {
 
                 <div className="mb-8 mx-8 md:w-2/6 md:mt-2">
                     <h3 className="text-xl font-bold my-2 md:text">Software Engineer</h3>
-                    <p className="text-gray-600 my-2">Company Name, Location</p>
-                    <p className="text-gray-700 my-2">January 2022 - Present</p>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel quae eos delectus harum sapiente obcaecati aliquam cum debitis officiis! Eum a ipsa ab fugiat accusamus exercitationem aperiam, autem obcaecati deleniti?</p>
+                    <p className="text-gray-600 my-2">Full Stack Developer , Softintra</p>
+                    <p className="text-gray-700 my-2">Feb 2024 - Present</p>
+                    <p>
+Improved user engagement by 35% with a mobile-friendly web platform, expanding user reach by 50%. Mentored 2 junior developers on MERN stack and SQL databases, increasing project efficiency by 20%. Collaborated with 8 developers to launch a program, generating $40k revenue in its first quarter.</p>
                 </div>
                 <div className="md:block hidden h-2/6 w-2/6 md:pt-16"> <img src={sphere} alt="" /></div>  
                 <div className="ml-24 mb-7 w-2/6  md:hidden"><img src={vsphere} alt="" /></div>
 
                 <div className="mb-8 mx-8 md:w-2/6 md:mt-56 md:pl-5">
-                    <h3 className="text-xl font-bold my-2 md:text">Software Engineer</h3>
-                    <p className="text-gray-600 my-2">Company Name, Location</p>
-                    <p className="text-gray-700 my-2">January 2022 - Present</p>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel quae eos delectus harum sapiente obcaecati aliquam cum debitis officiis! Eum a ipsa ab fugiat accusamus exercitationem aperiam, autem obcaecati deleniti?</p>
-                </div>
+                    <h3 className="text-xl font-bold my-2 md:text">Intern</h3>
+                    <p className="text-gray-600 my-2">Backend Developer, Softintra</p>
+                    <p className="text-gray-700 my-2">June 2023 - Aug 2023</p>
+<p>
+Implemented JWT, zod and bcrypt with Node.js for secure login and session control, reducing breaches by 75%. Collaborated for clear project requirements, delivering solutions exceeding client expectations by 20%. Developed 10+ web apps with HTML, CSS, and JS, increasing user engagement by 30%.</p>                </div>
             </div>
         </div>
     );
